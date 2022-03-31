ce0b8003c4cbc2aba45b9081908da4f74d48ef86
shspeed <shantcholakian@hotmail.com>
2022-03-31 15:44:25 -0400

    application routes

--- routes/application.js
+++ routes/application.js
@@ -1,14 +1,65 @@
-//routes for application
+//routes for parent student tutor application
 
 const router = require('express').Router();
 const checkAuth = require("../checkAuth.js");
 
 
-//example route that checks auth beofore starting 
+//empty routes that check auth beofore starting 
 router.get('/parent', checkAuth, (req, res) => {
+    const {  
+        firstName,
+        lastName,
+        phoneNumber,
+        email,
+        address,
+        relation,
+        city,
+        province 
+    } = req.body
+
     res.json({
         message: "private message"
     })
 });
+
+router.get('/student', checkAuth, (req, res) => {
+    const {  
+        firstName,
+        lastName,
+        phoneNumber,
+        email,
+        birthdate,
+        gender,
+        allergies,
+        grade,
+    } = req.body
+    
+    res.json({
+        message: "private message"
+    })
+});
+
+router.get('/tutor', checkAuth, (req, res) => {
+    const {  
+        firstName,
+        lastName,
+        phoneNumber,
+        email,
+        birthdate,
+        gender,
+        allergies,
+        address,
+        city,
+        province,
+        wasTutor,
+        eligibleToWork,
+        over18 
+    } = req.body
+    
+    res.json({
+        message: "private message"
+    })
+});
+
 
 module.exports = router;
