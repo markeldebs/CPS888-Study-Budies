7dda225c709b4c61536965c36b6a71d97fec60dc
shspeed <shantcholakian@hotmail.com>
2022-04-07 10:42:09 -0400

    fixed merge

--- routes/application.js
+++ routes/application.js
@@ -16,185 +16,164 @@ const pool = new Pool({
   });
 
 
-router.post('/dropoff', async (req, res) => {
-    const {  
-        firstName,
-        lastName,
-        parentEmail,
-        day,
-        time,
-        
-        
-    } = req.body
-    
-  //const values2 = [ran, firstName, lastName, parentEmail, day, time];
-    
-    const ran = [Math.floor(Math.random() * 1000)];
-    //const text = 'SELECT 1 FROM "Client" WHERE "Username" = \'' + username + "'";
-
-    const query1 = `INSERT INTO "DropOffStudent" ("Authentication_ID", "Student_First_Name","Student_Last_Name", "Parent_Email", "Date", "Time") VALUES ('${ran}', '${firstName}', '${lastName}', '${parentEmail}', '${day}', '${time}')`;
-
-    //console.log(query1);    test to see if query prints correctly
-    
-    pool.query(query1, (err, result) => {
-        if(err) {
-            console.log(err);
-            return res.status(422).json({
-                errors: [
-                  {
-                    msg: "Invalid Credentials",
-                  },
-                ],
-              });  
-        }
-        else{
-               // Runs for loop for duration of array.
-                //console.log(result.rows[i]);                     // Returns Firstnames of all participants
-                 // Conditional statement for if first name is Mark
-                    console.log("Dropped Off")
-                    //res.setHeader('55', 'Status')
-                    res.send({ message: "Dropped Off" })
-                    
-                        console.log(err);
-                        //res.setHeader('55', 'Status')
-                        console.log("Student not found")
-                        //res.send({message: "Student not found"});
-                  
-        }
-
-
 //student route 
 router.post('/student', async (req, res) => {
-    const {
-        firstName,
-        lastName,
-        phoneNumber,
-        email,
-        birthdate,
-        gender,
-        allergies,
-        grade,
-        token
-    } = req.body
+  const {
+      firstName,
+      lastName,
+      phoneNumber,
+      email,
+      birthdate,
+      gender,
+      allergies,
+      grade,
+      token
+  } = req.body
 
-    const jwtuser = await jwt.verify(token, "secret-key-studybuddies")
-    username = jwtuser.username;
+  const jwtuser = await jwt.verify(token, "secret-key-studybuddies")
+  username = jwtuser.username;
 
-    const text7 =
-        'INSERT INTO "Student" ("Student_ID","Parent_ID","FirstName","LastName","StudentEmailAddress", "BirthDate", "Gender", "Allergies", "Grade") VALUES((SELECT MAX("Student_ID")+1 FROM "Student"), (SELECT "Parent_ID" FROM "Parent", "Client" WHERE "Parent"."Client_ID" = "Client"."Client_ID" AND "Username" = $1), $2, $3, $4, $5, $6, $7, $8)';
-    const values7 = [username, firstName, lastName, email, birthdate, gender, allergies, grade];
-    await pool.query(text7, values7, (err, res) => {
-        if (err) {
-          //console.log(err.stack)
-        } else {
-          console.log(res.rows[0]);
-        }
-      });
-      // promise
-      pool
-        .query(text7, values7)
-        .then((res) => {
-          console.log(res.rows[0]);
-        })
-        .catch((e) => console.error(e.stack));
-      // async/await
-      try {
-        const res = await pool.query(text7, values7);
-        console.log(res.rows[0]);
-      } catch (err) {
+  const text7 =
+      'INSERT INTO "Student" ("Student_ID","Parent_ID","FirstName","LastName","StudentEmailAddress", "BirthDate", "Gender", "Allergies", "Grade") VALUES((SELECT MAX("Student_ID")+1 FROM "Student"), (SELECT "Parent_ID" FROM "Parent", "Client" WHERE "Parent"."Client_ID" = "Client"."Client_ID" AND "Username" = $1), $2, $3, $4, $5, $6, $7, $8)';
+  const values7 = [username, firstName, lastName, email, birthdate, gender, allergies, grade];
+  await pool.query(text7, values7, (err, res) => {
+      if (err) {
         //console.log(err.stack)
+      } else {
+        console.log(res.rows[0]);
       }
+    });
+    // promise
+    pool
+      .query(text7, values7)
+      .then((res) => {
+        console.log(res.rows[0]);
+      })
+      .catch((e) => console.error(e.stack));
+    // async/await
+    try {
+      const res = await pool.query(text7, values7);
+      console.log(res.rows[0]);
+    } catch (err) {
+      //console.log(err.stack)
+    }
 
 
-    res.json({
-        message: "True"
-    })
+  res.json({
+      message: "True"
+  })
 });
+
+
+router.post('/dropoff', async (req, res) => {
+  const {  
+      firstName,
+      lastName,
+      parentEmail,
+      day,
+      time,
+      
+      
+  } = req.body
+  
+//const values2 = [ran, firstName, lastName, parentEmail, day, time];
+  
+  const ran = [Math.floor(Math.random() * 1000)];
+  //const text = 'SELECT 1 FROM "Client" WHERE "Username" = \'' + username + "'";
+
+  const query1 = `INSERT INTO "DropOffStudent" ("Authentication_ID", "Student_First_Name","Student_Last_Name", "Parent_Email", "Date", "Time") VALUES ('${ran}', '${firstName}', '${lastName}', '${parentEmail}', '${day}', '${time}')`;
+
+  //console.log(query1);    test to see if query prints correctly
+  
+  pool.query(query1, (err, result) => {
+      if(err) {
+          console.log(err);
+          return res.status(422).json({
+              errors: [
+                {
+                  msg: "Invalid Credentials",
+                },
+              ],
+            });  
+      }
+      else{
+             // Runs for loop for duration of array.
+              //console.log(result.rows[i]);                     // Returns Firstnames of all participants
+               // Conditional statement for if first name is Mark
+                  console.log("Dropped Off")
+                  //res.setHeader('55', 'Status')
+                  res.send({ message: "Dropped Off" })
+                  
+                      console.log(err);
+                      //res.setHeader('55', 'Status')
+                      console.log("Student not found")
+                      //res.send({message: "Student not found"});
+                
+      }
+  })
+});
+
 
 
 router.post('/pickup', async (req, res) => {
-    const {  
-        code,
-    } = req.body
-    
- release = 0;
-    
-    //const text = 'SELECT 1 FROM "Client" WHERE "Username" = \'' + username + "'";
+  const {  
+      code,
+  } = req.body
+  
+release = 0;
+  
+  //const text = 'SELECT 1 FROM "Client" WHERE "Username" = \'' + username + "'";
 
-    const query2 = `SELECT "Authentication_ID", "Student_First_Name", "Student_Last_Name" FROM "DropOffStudent"`;
+  const query2 = `SELECT "Authentication_ID", "Student_First_Name", "Student_Last_Name" FROM "DropOffStudent"`;
 
-    //console.log(query1);    test to see if query prints correctly
-    
-    pool.query(query2, (err, result) => {
-        if(err) {
-            console.log(err);
-            return res.status(422).json({
-                errors: [
-                  {
-                    msg: "Invalid Credentials",
-                  },
-                ],
-              });
-        }
-        else{
-            for (i = 0; i < result.rowCount ; i++) {           // Runs for loop for duration of array.
-                //console.log(result.rowCount);                     // Returns Firstnames of all participants
-                //console.log(result);
-                if (result.rows[i].Authentication_ID == code){       // Conditional statement for if code matched
-                    console.log("Student Released")
-                    //res.setHeader('55', 'Status')
-                    //res.send({ message: "Student released" })
-                    //break;
-                    release = 1;
-                    break;
-                }
-                else{
-                        console.log(err);
-                        //res.setHeader('55', 'Status')
-                        console.log("Student not found")
-                        //res.send({message: "Wrong Code"})
-                        //res.send({message: "Student not found"});
-                        release = 0;
-                        //break;
-                    }
-            }
-            if (release == 1 ){                    
-            //res.setHeader('55', 'Status')
-            res.send({ message: "Student released" })
-            release = 0;
-            }
-            else {
-                //res.send({message: "Wrong Code"})
-                res.send({message: "Student not found"});
-            }
-        }
-    }) 
+  //console.log(query1);    test to see if query prints correctly
+  
+  pool.query(query2, (err, result) => {
+      if(err) {
+          console.log(err);
+          return res.status(422).json({
+              errors: [
+                {
+                  msg: "Invalid Credentials",
+                },
+              ],
+            });
+      }
+      else{
+          for (i = 0; i < result.rowCount ; i++) {           // Runs for loop for duration of array.
+              //console.log(result.rowCount);                     // Returns Firstnames of all participants
+              //console.log(result);
+              if (result.rows[i].Authentication_ID == code){       // Conditional statement for if code matched
+                  console.log("Student Released")
+                  //res.setHeader('55', 'Status')
+                  //res.send({ message: "Student released" })
+                  //break;
+                  release = 1;
+                  break;
+              }
+              else{
+                      console.log(err);
+                      //res.setHeader('55', 'Status')
+                      console.log("Student not found")
+                      //res.send({message: "Wrong Code"})
+                      //res.send({message: "Student not found"});
+                      release = 0;
+                      //break;
+                  }
+          }
+          if (release == 1 ){                    
+          //res.setHeader('55', 'Status')
+          res.send({ message: "Student released" })
+          release = 0;
+          }
+          else {
+              //res.send({message: "Wrong Code"})
+              res.send({message: "Student not found"});
+          }
+      }
+  }) 
 });
 
-
-
-
-router.get('/tutor', checkAuth, (req, res) => {
-    const {  
-        firstName,
-        lastName,
-        phoneNumber,
-        email,
-        birthdate,
-        gender,
-        allergies,
-        address,
-        city,
-        province,
-        wasTutor,
-        eligibleToWork,
-        over18 
-    } = req.body
-    
-    res.json({
-        message: "private message"
-    })
-});
 
 
 // //testing route
