60815b1fcf4d31e4bc6eb210e186af041d14ffe9
shspeed <shantcholakian@hotmail.com>
2022-04-09 20:53:42 -0400

    student fix

--- routes/shant.js
+++ routes/shant.js
@@ -32,10 +32,32 @@ router.post('/student', async (req, res) => {
     const jwtuser = await jwt.verify(token, "secret-key-studybuddies")
     username = jwtuser.username;
 
+    //get parent id
+    const query7 = {
+        text: 'SELECT "Parent_ID" FROM "Parent", "Client" WHERE "Parent"."Client_ID" = "Client"."Client_ID" AND "Username" = $1',
+        values: [username],
+    }
+    // callback
+    pool.query(query7, (err, res) => {
+        if (err) {
+            console.log(err.stack)
+            parentID = res.rows[0].Parent_ID;
+        } else {
+            console.log(res.rows[0]);
+            parentID = res.rows[0].Parent_ID;
+        }
+    })
+    // promise
+    await pool
+        .query(query7)
+        .then(res => console.log(res.rows[0]))
+        .catch(e => console.error(e.stack))
+
+
     //query
     const text7 =
-        'INSERT INTO "Student" ("Student_ID","Parent_ID","FirstName","LastName","StudentEmailAddress", "BirthDate", "Gender", "Allergies", "Grade") VALUES((SELECT MAX("Student_ID")+1 FROM "Student"), (SELECT "Parent_ID" FROM "Parent", "Client" WHERE "Parent"."Client_ID" = "Client"."Client_ID" AND "Username" = $1), $2, $3, $4, $5, $6, $7, $8)';
-    const values7 = [username, firstName, lastName, email, birthdate, gender, allergies, grade];
+        'INSERT INTO "Student" ("Student_ID","Parent_ID","FirstName","LastName","StudentEmailAddress", "BirthDate", "Gender", "Allergies", "Grade") VALUES((SELECT MAX("Student_ID")+1 FROM "Student"), $1, $2, $3, $4, $5, $6, $7, $8)';
+    const values7 = [parentID, firstName, lastName, email, birthdate, gender, allergies, grade];
     await pool.query(text7, values7, (err, res) => {
         if (err) {
             //console.log(err.stack)
